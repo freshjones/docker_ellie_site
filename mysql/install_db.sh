068efef95069fc4871f2c320cb9adc2dc6711616
William Jones <billy@freshjones.com>
2015-01-26 14:45:05 -0500

    adding mysql to the dockerfile

--- mysql/install_db.sh
+++ mysql/install_db.sh
@@ -7,13 +7,13 @@ mysqladmin -u root password welcome
 mysql -uroot -pwelcome -e "CREATE DATABASE elliesite;"
 mysql -uroot -pwelcome -e "GRANT ALL PRIVILEGES ON elliesite.* TO 'admin'@'localhost' IDENTIFIED BY 'welcome'; FLUSH PRIVILEGES;"
 
-echo "cd into app"
-cd /app/laravel
+#echo "cd into app"
+#cd /app/laravel
 
-echo "run migration script"
-php artisan migrate
+#echo "run migration script"
+#php artisan migrate
 
-echo "run seed script"
-php artisan db:seed
+#echo "run seed script"
+#php artisan db:seed
 
 mysqladmin -uroot -pwelcome shutdown
